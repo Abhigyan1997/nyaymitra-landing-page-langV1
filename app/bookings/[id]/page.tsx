f00ca82058a72550d2748eb0a908f18eea369711
Abhigyan1997 <alokabhigyan65@gmail.com>
2025-07-13 15:20:58 +0530

    bug in get all booking page

--- app/bookings/[id]/page.tsx
+++ app/bookings/[id]/page.tsx
@@ -150,7 +150,7 @@ export default function BookingDetails() {
         const fetchBooking = async () => {
             try {
                 const token = localStorage.getItem("token")
-                const response = await axios.get(`ttps://nyaymitra-backend.onrender.com/api/v1/booking/${bookingId}`, {
+                const response = await axios.get(`https://nyaymitra-backend.onrender.com/api/v1/booking/${bookingId}`, {
                     headers: {
                         Authorization: `Bearer ${token}`,
                     },
