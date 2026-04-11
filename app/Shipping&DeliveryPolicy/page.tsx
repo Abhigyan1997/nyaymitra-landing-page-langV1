1f4a2ee6d9e93e0b45fa009117ece91f3a391575
Abhigyan1997 <alokabhigyan65@gmail.com>
2026-04-11 12:31:59 +0530

    Shiiping policy chnage

--- app/Shipping&DeliveryPolicy/page.tsx
+++ app/Shipping&DeliveryPolicy/page.tsx
@@ -92,7 +92,7 @@ export default function ShippingPolicyPage() {
                             href="mailto:support@nyaymitra.tech"
                             className="text-blue-600 hover:underline font-medium"
                         >
-                            contact@nyaymitra.tech
+                            support@nyaymitra.tech
                         </a>
                     </div>
                 </section>
